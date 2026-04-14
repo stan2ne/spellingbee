ba339803d6fb60b4221b5b84a950242822eb34b0
sawas <savasboluk@cloud.com>
2026-04-14 23:44:00 +0300

    test için insight eklendi

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
