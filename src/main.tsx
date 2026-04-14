4e800890825ec3db5019c0eba480a2241dbc06c6
sawas <savasboluk@cloud.com>
2026-04-14 23:56:34 +0300

    feat: add Vercel SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,6 @@ import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
